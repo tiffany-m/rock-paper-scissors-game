5ba4e34cb63f2b8fc82bf20f732bef90c573e29b
Tiffany M <67019104+tiffany-m@users.noreply.github.co>
2023-01-17 11:13:05 -0500

    Add function to play game, console.logs, comments
    Added main function to play game called "game()."  Also added some
    console.log()'s to add funcationality and make it easier to debug.
    Added comments to get a clearer picture of what was happening.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+let computerPoints = 0;
+let playerPoints = 0;
+let round = 1;
+
+// get computer choice
 function getComputerChoice() {
     let computerOptions = ['Rock', 'Paper', 'Scissors'];
     let randomNumber = Math.floor(Math.random() * 2);
@@ -6,6 +11,7 @@ function getComputerChoice() {
     return computerChoice;
 }
 
+// get player choice
 function getPlayerChoice() {
     let playerChoiceStr = prompt('Please select "rock," "paper" or "scissors" to start the game!', 'rock, paper or scissors');
     let toUpper = playerChoiceStr.charAt(0).toUpperCase();
@@ -15,18 +21,46 @@ function getPlayerChoice() {
     return playerChoice;
 }
 
-let computerSelection = getComputerChoice();
-let playerSelection = getPlayerChoice();
-
-
+// play one round of game
 function playRound(computer, player) {
     if (computer == player) {
-        return `It's a tie! You both chose ${computer}!`;
+        computerPoints++;
+        playerPoints++;
+        return console.log(`It's a tie! You both chose ${computer}! Points for each of you!`);
     } else if (computer == "rock" && player == "scissors" || computer == "scissors" && player == "paper" || computer == "paper" && player == "rock") {
-        return `You lose! ${computer} beats ${player}!`;
+        computerPoints++;
+        return console.log(`You lose! ${computer} beats ${player}!`);
     } else {
-        return `You Win! ${player} beats ${computer}!`;
+        playerPoints++;
+        return console.log(`You Win! ${player} beats ${computer}!`);
     }
 }
 
-console.log(playRound(computerSelection, playerSelection));
+function game() {
+    // set variables
+    let computerSelection = getComputerChoice();
+    let playerSelection = getPlayerChoice();
+    
+    console.log(`round: ${round}, computer choice: ${computerSelection}, player choice: ${playerSelection}`);
+
+    for (let i = 0; i < 5; i++) {
+        // play one round
+        playRound(computerSelection, playerSelection);
+
+        // new choices
+        computerSelection = getComputerChoice();
+        playerSelection = getPlayerChoice();
+
+        round++;
+
+        console.log(`round: ${round}, computer choice: ${computerSelection}, player choice: ${playerSelection}, current score: computer ${computerPoints} / player ${playerPoints}`)
+    }
+
+    if (computerPoints == playerPoints) {
+        return console.log("The 5 round game was a tie!");
+    } else if (computerPoints > playerPoints) {
+        return console.log("The computer has won this 5 round game!");
+    } else {
+        return console.log("You have won this 5 round game!");
+    }
+}
