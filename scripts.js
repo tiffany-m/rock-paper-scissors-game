ff12308d480575b677c499bdcd51d5934922c289
Tiffany M <67019104+tiffany-m@users.noreply.github.co>
2023-01-17 13:50:24 -0500

    Fix logic for playRound(), game()

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// set variables
 let computerPoints = 0;
 let playerPoints = 0;
 let round = 1;
@@ -5,7 +6,7 @@ let round = 1;
 // get computer choice
 function getComputerChoice() {
     let computerOptions = ['Rock', 'Paper', 'Scissors'];
-    let randomNumber = Math.floor(Math.random() * 2);
+    let randomNumber = Math.floor(Math.random() * computerOptions.length);
     let computerChoice = computerOptions[randomNumber];
 
     return computerChoice;
@@ -13,7 +14,7 @@ function getComputerChoice() {
 
 // get player choice
 function getPlayerChoice() {
-    let playerChoiceStr = prompt('Please select "rock," "paper" or "scissors" to start the game!', 'rock, paper or scissors');
+    let playerChoiceStr = prompt('Please select "rock," "paper" or "scissors" to start the game!');
     let toUpper = playerChoiceStr.charAt(0).toUpperCase();
     let toLower = playerChoiceStr.slice(1).toLowerCase();
     let playerChoice = toUpper + toLower;
@@ -26,13 +27,13 @@ function playRound(computer, player) {
     if (computer == player) {
         computerPoints++;
         playerPoints++;
-        return console.log(`It's a tie! You both chose ${computer}! Points for each of you!`);
-    } else if (computer == "rock" && player == "scissors" || computer == "scissors" && player == "paper" || computer == "paper" && player == "rock") {
+        return console.log(`It's a tie! You both chose ${computer}! Points for each of you! Current Score: Computer ${computerPoints} / Player ${playerPoints}`);
+    } else if (computer == "Rock" && player === "Scissors" || computer === "Scissors" && player === "Paper" || computer === "Paper" && player === "Rock") {
         computerPoints++;
-        return console.log(`You lose! ${computer} beats ${player}!`);
+        return console.log(`You lose! ${computer} beats ${player}! Current Score: Computer ${computerPoints} / Player ${playerPoints}`);
     } else {
         playerPoints++;
-        return console.log(`You Win! ${player} beats ${computer}!`);
+        return console.log(`You Win! ${player} beats ${computer}! Current Score: Computer ${computerPoints} / Player ${playerPoints}`);
     }
 }
 
@@ -40,27 +41,27 @@ function game() {
     // set variables
     let computerSelection = getComputerChoice();
     let playerSelection = getPlayerChoice();
-    
-    console.log(`round: ${round}, computer choice: ${computerSelection}, player choice: ${playerSelection}`);
 
     for (let i = 0; i < 5; i++) {
+        console.log(`Round: ${round}\n“Rock, paper, scissors, shoot!”\nComputer Choice: ${computerSelection}, Player Choice: ${playerSelection}`)
+
         // play one round
         playRound(computerSelection, playerSelection);
 
-        // new choices
+        // new choices for next round
         computerSelection = getComputerChoice();
         playerSelection = getPlayerChoice();
 
         round++;
 
-        console.log(`round: ${round}, computer choice: ${computerSelection}, player choice: ${playerSelection}, current score: computer ${computerPoints} / player ${playerPoints}`)
-    }
-
-    if (computerPoints == playerPoints) {
-        return console.log("The 5 round game was a tie!");
-    } else if (computerPoints > playerPoints) {
-        return console.log("The computer has won this 5 round game!");
-    } else {
-        return console.log("You have won this 5 round game!");
+        if (round === 5) {
+            if (computerPoints == playerPoints) {
+                return console.log(`The 5 round game was a tie! You both have ${computerPoints} points`);
+            } else if (computerPoints > playerPoints) {
+                return console.log(`The computer has won this 5 round game! Final Score: Computer ${computerPoints} / Player ${playerPoints}`);
+            } else {
+                return console.log(`You have won this 5 round game! Current Score: Computer ${computerPoints} / Player ${playerPoints}`);
+            }
+        }
     }
 }
