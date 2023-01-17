158525e7176f46a3b1ac6261f33e0e26d50172b5
Tiffany M <67019104+tiffany-m@users.noreply.github.co>
2023-01-17 14:50:56 -0500

    Fix comments and console.logs to be more clear

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// set variables
+// set counter variables
 let computerPoints = 0;
 let playerPoints = 0;
 let round = 1;
@@ -15,6 +15,7 @@ function getComputerChoice() {
 // get player choice
 function getPlayerChoice() {
     let playerChoiceStr = prompt('Please select "rock," "paper" or "scissors" to start the game!');
+    // fix/make sure input for player has correct format
     let toUpper = playerChoiceStr.charAt(0).toUpperCase();
     let toLower = playerChoiceStr.slice(1).toLowerCase();
     let playerChoice = toUpper + toLower;
@@ -22,28 +23,31 @@ function getPlayerChoice() {
     return playerChoice;
 }
 
-// play one round of game
+// play one round of game by comparing choices of computer and player
 function playRound(computer, player) {
     if (computer == player) {
         computerPoints++;
         playerPoints++;
-        return console.log(`It's a tie! You both chose ${computer}! Points for each of you! Current Score: Computer ${computerPoints} / Player ${playerPoints}`);
+        return console.log(`It's a tie! You both chose ${computer}! Points for each of you!\nCurrent Score: Computer ${computerPoints} / Player ${playerPoints}`);
     } else if (computer == "Rock" && player === "Scissors" || computer === "Scissors" && player === "Paper" || computer === "Paper" && player === "Rock") {
         computerPoints++;
-        return console.log(`You lose! ${computer} beats ${player}! Current Score: Computer ${computerPoints} / Player ${playerPoints}`);
+        return console.log(`You lose! ${computer} beats ${player}!\nCurrent Score: Computer ${computerPoints} / Player ${playerPoints}`);
     } else {
         playerPoints++;
-        return console.log(`You Win! ${player} beats ${computer}! Current Score: Computer ${computerPoints} / Player ${playerPoints}`);
+        return console.log(`You Win! ${player} beats ${computer}!\nCurrent Score: Computer ${computerPoints} / Player ${playerPoints}`);
     }
 }
 
+// play 5 round game
 function game() {
     // set variables
     let computerSelection = getComputerChoice();
     let playerSelection = getPlayerChoice();
 
     for (let i = 0; i < 5; i++) {
-        console.log(`Round: ${round}\n“Rock, paper, scissors, shoot!”\nComputer Choice: ${computerSelection}, Player Choice: ${playerSelection}`)
+        console.log(`-------------- ROUND ${round} --------------
+        \n“Rock, paper, scissors, shoot!”
+        \nComputer Choice: ${computerSelection}, Player Choice: ${playerSelection}`)
 
         // play one round
         playRound(computerSelection, playerSelection);
@@ -55,13 +59,11 @@ function game() {
         round++;
     }
 
-    if (round === 5) {
-        if (computerPoints == playerPoints) {
-            return console.log(`The 5 round game was a tie! You both have ${computerPoints} points`);
-        } else if (computerPoints > playerPoints) {
-            return console.log(`The computer has won this 5 round game! Final Score: Computer ${computerPoints} / Player ${playerPoints}`);
-        } else {
-            return console.log(`You have won this 5 round game! Current Score: Computer ${computerPoints} / Player ${playerPoints}`);
-        }
+    if (computerPoints == playerPoints) {
+        return console.log(`The 5 round game was a tie! You both have ${computerPoints} points`);
+    } else if (computerPoints > playerPoints) {
+        return console.log(`The computer has won this 5 round game!\nFinal Score: Computer ${computerPoints} / Player ${playerPoints}`);
+    } else {
+        return console.log(`You have won this 5 round game!\nCurrent Score: Computer ${computerPoints} / Player ${playerPoints}`);
     }
 }
